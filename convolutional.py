119c9d14acfc1c8916b7242ad3a6fd4f81c71e4c
Laura Datta <50656723+lauradatta@users.noreply.github.com>
2020-02-19 13:13:29 +0100

    Add files via upload

---
+++ convolutional.py
@@ -0,0 +1,53 @@
+##% run preprocessing module
+from preprocessing import X_train, X_test, y_train_hot, y_test_hot
+
+#import packages
+# Import packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import pickle
+from keras.models import Sequential
+from keras.layers import Conv1D, MaxPooling1D, LSTM, Dense
+from keras.optimizers import Adam
+import logging
+
+# Remove some unwanted warnings
+logging.getLogger('tensorflow').disabled = True
+
+#%% define the model
+
+def base_model():
+    model = Sequential()
+    model.add (Conv1D (8, 8, activation = 'relu', input_shape = (X_train.shape[1], 2)))
+    model.add (MaxPooling1D (8))
+    model.add (Conv1D (16, 8, activation = 'relu'))
+    model.add (MaxPooling1D (8))
+    model.add (Conv1D (32, (8), activation = 'relu'))
+    model.add (MaxPooling1D (8))
+    model.add (LSTM (64, return_sequences = True))
+    model.add (LSTM (64))
+    model.add (Dense (6, activation = 'softmax'))
+
+    optimizer = Adam (lr = 0.001, beta_1 = 0.9, beta_2 = 0.999, decay = 0, epsilon = (10**-8))
+    model.compile (loss = 'categorical_crossentropy',
+                   optimizer = optimizer,
+                   metrics = ['accuracy'])
+    return model
+
+
+model = base_model()
+model.summary()
+
+#%% # train the model
+
+model.fit(X_train,y_train_hot,
+          epochs=50,
+          batch_size=128,
+          verbose =2)
+
+#%% evaluate the model
+
+cat, test_acc = model.evaluate(X_test1, y_test_hot, batch_size=128)
+print("accuracy score on test set is:{}".format(round(test_acc, 3)))
