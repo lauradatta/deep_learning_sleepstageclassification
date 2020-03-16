1220869e01b4e88bb0cf9a1a0272c25c45f611f2
laura.datta <laura@datta-online.com>
2020-03-16 16:20:35 +0100

    fixed shape problems

--- 2cnn_raw.py
+++ 2cnn_raw.py
@@ -7,8 +7,8 @@ Created on Mon Mar  9 16:10:36 2020
 
 
 from keras.models import Sequential
-from keras.layers import Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, LSTM, Dense, Input, Dropout,Flatten, Average,  concatenate, Concatenate
-from keras.optimizers import Adam
+from keras.layers import Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, LSTM, Dense, Input, Dropout,Flatten, Average,  concatenate, Concatenate, Reshape
+from keras.optimizers import Adam, rmsprop
 from keras import Model
 import logging
 
@@ -36,14 +36,17 @@ def get_model():
     layer2 = MaxPooling1D (pool_size = 4, strides = 4, padding = 'same') (layer2)
     layer2 = Flatten()(layer2)
     
-    
+       
     comb = concatenate([layer1, layer2])
+    comb = Dropout (0.5)(comb)
+    comb = Reshape((1,2560))(comb)
+    
     comb = LSTM(64, return_sequences = True) (comb)
-    comb = LSTM (64)(comb)
+    comb = LSTM (32)(comb)
     
     output_layer= Dense(6, activation = 'softmax')(comb)
     
-    optimizer = Adam (lr = 0.001, beta_1 = 0.9, beta_2 = 0.999, decay = 0, epsilon = (10 ** -8))
+    optimizer = rmsprop()
     model = Model (inputs = input_raw, outputs = output_layer)
     model.compile (loss = 'categorical_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
     
@@ -53,6 +56,36 @@ def get_model():
 
 model = get_model()
 model.summary()
+
+#%%
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(maxlen_input, ctable.num_tokens))
+encoder_rnn_layer = GRU(hidden_size, return_state=True)
+# We discard the output of the layer and only keep the states.
+_, encoder_state = encoder_rnn_layer(encoder_inputs)
+
+### UPDATE CODE HERE ###
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, ctable.num_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_rnn_layer = GRU(hidden_size, return_sequences=True, return_state=True)
+decoder_outputs, _ = decoder_rnn_layer(decoder_inputs,
+                                       initial_state=encoder_state)
+decoder_dense = Dense(ctable.num_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+### END ###
+
+# Define the model that will turn
+# `encoder_inputs` & `decoder_inputs` into `decoder_outputs`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+model.summary()
+
+
 
 #%%
 hist = model.fit(raw_train, y_train_hot,
