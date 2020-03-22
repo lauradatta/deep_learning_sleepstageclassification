d1fec3295e4aa5160246f0d4a75d298ff0e20de6
maoam1994 <41284672+maoam1994@users.noreply.github.com>
2020-03-22 15:03:41 +0100

    final version - accuracy on testset: 0.7206

--- 2CNN_LSTM_final.py
+++ 2CNN_LSTM_final.py
@@ -1,4 +1,15 @@
-## import
+'''
+final version of the deep learning assignment
+
+###### students:
+Alicja Ciuńczyk (2030826)
+Laura Gärtner (2036336)
+Maurice Bijl (2034154)
+
+###### date:
+2020/03/22 (YYYY/MM/DD)'''
+
+#### Import packages ####
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
@@ -9,38 +20,42 @@ from keras import optimizers
 from keras import Model
 import keras.backend as K
 from keras.utils import to_categorical
+import tensorflow as tf
 
-#### preprocessing part ####
-
-## set random seed for SMOTE
+#### Set random seeds for reproducibility ####
+np.random.seed(123)
+tf.random.set_seed(123)
 sm = SMOTE(random_state=42)
 
-# Load data
+#### Preprocessing part ####
+
+## Load data
 with open ('data/Data_Raw_signals.pkl', 'rb') as f:
     raw = pickle.load (f)
 
 with open ('data/Data_Spectrograms.pkl', 'rb') as f:
     spect = pickle.load (f)
 
-#load test data with no labels
+## Load test data with no labels
 with open ('test_data/Test_Raw_signals_no_labels.pkl', 'rb') as f:
     raw_test = pickle.load (f)
 
 with open ('test_data/Test_Spectrograms_no_labels.pkl', 'rb') as f:
     spec_test = pickle.load (f)
 
-#### split labels from features
+## Split labels from features
 raw_train = np.array (raw [0])  # full data
 
 spec_train = np.array (spect [0])  # full data
 y_train = np.array (raw [1])  # labels
 
-## remove noisy samples
+## Remove noisy samples
 raw_train.shape
 elct1 = raw_train[:,0]
 elct2 = raw_train[:,1]
 too_high = []
-getal = 2000
+threshold = 2000
+
 for i in range(elct1.shape[0]):
     temp = elct1[i]
     temp1 = elct2[i]
@@ -48,52 +63,52 @@ for i in range(elct1.shape[0]):
     numbermax1 = temp1[np.argmax (temp1)]
     numbermin = temp[np.argmin(temp)]
     numbermin1 = temp1[np.argmin (temp1)]
-    if numbermax > getal or numbermin <(-getal) or numbermax1 > getal or numbermin1 <(-getal):
+    if numbermax > threshold or numbermin <(-threshold) or numbermax1 > threshold or numbermin1 <(-threshold):
         too_high.append(i)
 
 raw_train = np.delete(raw_train, too_high, axis = 0)
 spec_train = np.delete(spec_train, too_high, axis = 0)
 y_train = np.delete(y_train, too_high, axis = 0)
 
-# hot encode the labels
+## Hot encode the labels
 y_train_hot = to_categorical (y_train, num_classes = 6)
 
-## extract data for 1st and 2nd electrode - raw signals
+## Extract data for 1st and 2nd electrode - raw signals
 train_raw_1 = raw_train[:, 0]  # first electrode
 train_raw_2 = raw_train[:, 1]  # second electrode
 
 test_raw_1 = raw_test[0][:, 0] # first electrode
 test_raw_2 = raw_test[0][:, 1] # second electrode
 
-## extract data for 1st and 2nd electrode - spectogram
+## Extract data for 1st and 2nd electrode - spectogram
 train_spec_1 = spec_train [:, 0]  # first electrode
 train_spec_2 = spec_train [:, 1]  # second electrode
 
 test_spec_1 = spec_test[0][:, 0]  # first electrode
 test_spec_2 = spec_test[0][:, 1]  # second electrode
 
-# standardize raw signal 1
+## Standardize raw signal 1
 scaler_sig1 = StandardScaler()
 train_raw_1 = scaler_sig1.fit_transform (train_raw_1)
 test_raw_1 = scaler_sig1.transform(test_raw_1)
 
-# standardize raw signal 2
+## Standardize raw signal 2
 scaler_sig2 = StandardScaler()
 train_raw_2 = scaler_sig2.fit_transform (train_raw_2)
 test_raw_2 = scaler_sig2.transform(test_raw_2)
 
-## oversample the data with SMOTE
+## Oversample the data with SMOTE
 X_smote_raw, y_smote_raw = sm.fit_resample(train_raw_1, y_train_hot)
 X_smote_raw2, y_smote_raw2 = sm.fit_resample(train_raw_2, y_train_hot)
 
-#concatenate the raw signals and reshape to shape (15375, 3000, 2)
+## Concatenate the raw signals and reshape to shape (15375, 3000, 2)
 raw_train = np.stack ((X_smote_raw, X_smote_raw2), axis = 1)
 raw_train = raw_train.reshape (raw_train.shape[0],raw_train.shape[2], raw_train.shape[1])
 
 raw_test = np.stack ((test_raw_1, test_raw_2), axis = 1)
 raw_test = raw_test.reshape (raw_test.shape[0],raw_test.shape[2], raw_test.shape[1])
 
-## this creates 100 standardizers (for every 0.5 hz)
+## This creates 100 standardizers (for every 0.5 hz)
 scalers1 = {}
 for i in range(train_spec_1.shape[1]):
     scalers1[i] = StandardScaler()
@@ -106,7 +121,7 @@ for i in range(train_spec_2.shape[1]):
     train_spec_2[:, i, :] = scalers2[i].fit_transform(train_spec_2[:, i, :])
     test_spec_2 [:, i, :] = scalers2 [i].transform (test_spec_2 [:, i, :])
 
-## oversample the data with SMOTE
+## Oversample the data with SMOTE
 spec_train_1 = train_spec_1.reshape(train_spec_1.shape[0],3000)
 spec_train_2 = train_spec_2.reshape(train_spec_2.shape[0],3000)
 
@@ -116,7 +131,7 @@ X_smote_spec2, y_smote_spec2 = sm.fit_resample(spec_train_2, y_train_hot)
 X_smote_spec1 = X_smote_spec1.reshape(X_smote_spec1.shape[0],100,30)
 X_smote_spec2 = X_smote_spec2.reshape(X_smote_spec2.shape[0],100,30)
 
-#concatenate the spectograms and reshape to shape (15375, 100, 30, 2,)
+# Concatenate the spectograms and reshape to shape (15375, 100, 30, 2,)
 spec_train = np.stack((X_smote_spec1,X_smote_spec2), axis = 1)
 
 spec_train = spec_train.reshape(spec_train.shape[0],
@@ -130,12 +145,12 @@ spec_test = spec_test.reshape(spec_test.shape[0],
                               spec_test.shape[3],
                               spec_test.shape[1])
 
-## change y_train_hot to y_smote_raw
+## Change y_train_hot to y_smote_raw
 y_train_hot = y_smote_raw
 
 #### build model ####
-K.clear_session()
-## first part of the model (raw signals as input)
+
+## First part of the model (raw signals as input)
 input_raw = Input(shape =(raw_train.shape[1], 2))
 layer1 = Conv1D (8, 8, activation = 'relu') (input_raw)
 layer1 = MaxPooling1D (8) (layer1)
@@ -147,7 +162,7 @@ layer1 = MaxPooling1D (8) (layer1)
 layer1 = Dropout(0.3)(layer1)
 layer1 = Flatten()(layer1)
 
-## second part of the model (spectograms as input)
+## Second part of the model (spectograms as input)
 input_spec = Input(shape =(spec_train.shape[1], spec_train.shape[2], spec_train.shape[3]))
 layer2 = Conv2D(8,(3,3),activation = 'relu')(input_spec)
 layer2 = MaxPooling2D(2,2)(layer2)
@@ -159,12 +174,12 @@ layer2 = MaxPooling2D(2,2)(layer2)
 layer2 = Dropout(0.5)(layer2)
 layer2 = Flatten()(layer2)
 
-## concatenate the two part and reshape for the LSTM part
+## Concatenate the two part and reshape for the LSTM part
 comb = Concatenate ()([layer1, layer2])
 comb = Dropout(0.5)(comb)
 lstm_input = Reshape ((1, K.int_shape(comb)[1])) (comb)
 
-##LSTM part
+## LSTM part
 lstm_part = LSTM (64, return_sequences = True) (lstm_input)
 lstm_part = LSTM (32) (lstm_part)
 
@@ -172,25 +187,25 @@ lstm_part = LSTM (32) (lstm_part)
 dense_part = Dense(32, activation = 'relu') (lstm_part)
 output_layer = Dense (6, activation = 'softmax') (dense_part)
 
-## create model and compile with Adam optimizer
+## Create model and compile with Adam optimizer
 model = Model (inputs = [input_raw,input_spec], outputs = output_layer)
 optimizer = optimizers.Adam (lr = 0.001, beta_1 = 0.9, beta_2 = 0.999, decay = 0, epsilon = (10 ** -8)) ## ff naar kijken nog
 model.compile (loss = 'categorical_crossentropy',
                   optimizer = optimizer,
                   metrics = ['accuracy'])
 
-## fit the model
+## Fit the model
 model.fit([raw_train, spec_train], y_train_hot,
           epochs=200,
           batch_size=128,
           verbose =2)
 
-## predict the labels of the test data
+## Predict the labels of the test data
 y_pred = model.predict([raw_test, spec_test])
 y_pred = y_pred.argmax(axis=-1)
 y_pred = y_pred.astype(int)
 
-## save the labels as .txt file
+## Save the labels as .txt file
 with open('answer.txt', 'w') as f:
     for item in y_pred:
         f.write("%s\n" % item)
